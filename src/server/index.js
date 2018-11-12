864f91c10f37891a17d36a0ed8c2c8b9d950da86
Alexey <alexeyklimovich@gmail.com>
2018-11-12 22:54:19 +0300

    Implement Task

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const util = require('util');
-const exec = require('child_process').exec;
-const os = require('os');
-const pathname = './Lab2(1).docx';
 const app = express();
+const cors = require('cors');
+const multer = require('multer');
+const util = require('util');
+const { exec } = require('child_process');
 
-app.use(express.static('dist'));
+let filename = '';
 
-function getCommandLine() {
+const getCommandLine = () => {
   switch (process.platform) { 
      case 'darwin' : return 'open';
      case 'win32' : return 'start';
@@ -16,7 +16,27 @@ function getCommandLine() {
   }
 }
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'files/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+    filename = file.originalname;
+  }
+})
+const uploads = multer({storage: storage})
+console.log(filename)
+app.use(express.static('dist'));
+app.use(cors());
 
-exec(getCommandLine() + ' ' + pathname);
+
+app.get('/', (req, res) => {
+  res.send('Сервер')
+})
+
+app.post('/form', uploads.any(), (req, res) => {
+  exec(getCommandLine() + " " + __dirname + `/../../files/${filename}`, (error) => console.log(error));
+});
 
 app.listen(8080, () => console.log('Listening on port 8080!'));
